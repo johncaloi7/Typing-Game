c495b39f38d266060d06f66c73ccd45ad0819ef2
unknown <pinlac.johncarlo@gmail.com>
2023-04-12 12:28:27 +0800

    angular typing challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onChangeInput($event: Event) {
+    const { value } = $event.target as HTMLInputElement;
+
+    this.enteredText = value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return randomLetter === enteredLetter ? 'success' : 'danger';
+  }
 }
